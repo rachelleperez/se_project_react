12d88abe82e3407533665d6b823d123584f8fb87
Rachelle Perez <rachelleaperez@gmail.com>
2023-10-16 14:39:25 -0400

    moves Header to its own component

---
+++ src/Header/Header.js
@@ -0,0 +1,26 @@
+const Header = () => {
+    console.log('Header');
+
+    return (
+      <header className = 'header'>
+      <div className = 'header__logo'>
+        <div> 
+          <img src='./images/logo.svg' alt="logo" />
+        </div>
+        <div> Date </div>
+      </div>
+      <div className = 'header__avatar-logo'>
+        <div>
+          <button type= 'text'> Add New Clothes </button>
+        </div>
+        <div>Name</div>
+        <div>
+          <img src='./images/avatar.svg' alt="avatar" />
+        </div>
+      </div>
+      </header>
+    )
+
+}
+
+export default Header;
