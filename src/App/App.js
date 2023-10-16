c2b7e83ce00affb086807105bc61855251c28d8e
Rachelle Perez <rachelleaperez@gmail.com>
2023-10-16 15:02:00 -0400

    main's weather section skeleton

--- src/App/App.js
+++ src/App/App.js
@@ -7,6 +7,15 @@ function App() {
   return (
     <div>
     <Header/>
+    <main className='main'>
+      <section className='weather' id='weather'>
+        <div className='weather_info'>75F</div>
+        <img src= '/images/day/day_sunny.svg' className='weather_image'/>
+      </section>
+      <section id='card-section'>
+        card section
+      </section>
+    </main>
     </div>
   );
 }
