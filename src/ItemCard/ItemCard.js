8dec8580383469cdbf1076573439379a38762175
Rachelle Perez <rachelleaperez@gmail.com>
2023-10-16 16:10:24 -0400

    mvp itemcard done

--- src/ItemCard/ItemCard.js
+++ src/ItemCard/ItemCard.js
@@ -1,5 +1,12 @@
-const ItemCard = () => {
-    console.log('item card')
-}
+const ItemCard = ({x}) => {
+    return (
+        <div>
+            <div>
+                <img src={x.link} className='card_image'/>
+            </div>
+            <div className='card_name'>{x.name}</div>
+        </div>
+    );
+};
 
 export default ItemCard;
