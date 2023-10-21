7669db5b838413643ec2120f74d5971c1d1e48ca
Rachelle Perez <rachelleaperez@gmail.com>
2023-10-21 01:03:37 -0400

    added like heart with dummy logic

--- src/ItemCard/ItemCard.js
+++ src/ItemCard/ItemCard.js
@@ -1,12 +1,20 @@
 import './ItemCard.css';
 
-const ItemCard = ({ item, onSelectCard }) => {
+const ItemCard = ({ item, onSelectCard}) => {
+
+    // temp until logic for liked in next sprint(s)
+    const likeSrc = '/images/likeEmpty.svg'
+    const handleLike = (item) => {
+        console.log('clicked like heart');
+    }
+
     return (
         <div className='card'>
             <img src={item.link} className='card__image' onClick={() => onSelectCard(item)} />
             <div className='card__content'>
                 <div className='card__name'>
                     <span className='card__name-span'>{item.name}</span>
+                    <img src={likeSrc} className='card__like-button' onClick={() => handleLike(item)} />
                 </div>
             </div>
         </div>
