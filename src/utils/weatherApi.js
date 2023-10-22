1b2952365f95e2b22344a738f1d4f50d62cf2c26
Rachelle Perez <rachelleaperez@gmail.com>
2023-10-22 01:20:18 -0400

    catches api err in weather api

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -12,14 +12,18 @@ const APIkey = "4e13ef9a50584870e10fa9c152e319bb";
 export const getForecastWeather = () => {
   const weatherApi = fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
-  ).then((res) => {
-    // console.log(res);
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
+  )
+    .then((res) => {
+      // console.log(res);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Error: ${res.status}`);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   return weatherApi;
 };
 
