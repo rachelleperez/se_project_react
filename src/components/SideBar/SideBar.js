138bee06f11da562cb800ff6b2668600d6e60cf8
Rachelle Perez <rachelleaperez@gmail.com>
2023-12-30 20:27:07 -0500

    side bar in profile now populates current user's info

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,13 +1,16 @@
 import "./SideBar.css";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 const avatarImagePath = "/images/avatar.svg";
 
 const SideBar = () => {
+  const { currentUser } = useContext(CurrentUserContext);
   return (
     <div className="sidebar">
       <div className="sidebar__user">
-        <img className="sidebar__avatar" src={avatarImagePath} alt="user" />
-        <p className="sidebar__username">Terrence Tegegne</p>
+        <img className="sidebar__avatar" src={currentUser.avatar} alt="user" />
+        <p className="sidebar__username">{currentUser.name}</p>
       </div>
     </div>
   );
