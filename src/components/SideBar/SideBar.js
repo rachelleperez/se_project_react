5969f18b49fd85f03c53681e8964418541450251
Rachelle Perez <rachelleaperez@gmail.com>
2023-12-30 23:00:32 -0500

    avatar placeholder in sidebar too

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,14 +2,28 @@ import "./SideBar.css";
 import { useContext } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-const avatarImagePath = "/images/avatar.svg";
-
 const SideBar = () => {
   const { currentUser } = useContext(CurrentUserContext);
+
+  // manage optional avatar, extract first letter in case it's needed
+  const avatarAvailable = currentUser.avatar !== undefined;
+  const firstLetterName =
+    currentUser.avatar !== undefined && currentUser.name.length > 0
+      ? currentUser.name[0]
+      : "X"; // X placeholder
+
   return (
     <div className="sidebar">
       <div className="sidebar__user">
-        <img className="sidebar__avatar" src={currentUser.avatar} alt="user" />
+        {avatarAvailable ? (
+          <img
+            className="sidebar__avatar"
+            src={currentUser.avatar}
+            alt="user"
+          />
+        ) : (
+          <p className="sidebar__avatar-placeholder">{firstLetterName}</p>
+        )}
         <p className="sidebar__username">{currentUser.name}</p>
       </div>
     </div>
